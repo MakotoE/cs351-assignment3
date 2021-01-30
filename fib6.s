a2e768a85ca4572c73e03333c5744cb7a9586ce2
Makoto <makoto@emurasoft.com>
2021-01-29 20:08:00 -0800

    Makefile

--- fib6.s
+++ fib6.s
@@ -1,16 +1,16 @@
 .data:
-f0: .byte 0
-f1: .byte 1
+	f0: .byte 0
+	f1: .byte 1
 
 .global _start
 _start:
-ldr r0, =f0
-ldr r1, =f1
-add r0, r0, r1		; f_0 + f_1
-add r1, r0, r1		; f_2 + f_1
-add r0, r0, r1		; f_3 + f_2
-add r1, r0, r1		; f_4 + f_3
-add r0, r0, r1		; f_5 + f_4
-bx lr
+	ldr r0, =f0
+	ldr r1, =f1
+	add r0, r0, r1		// f_0 + f_1
+	add r1, r0, r1		// f_2 + f_1
+	add r0, r0, r1		// f_3 + f_2
+	add r1, r0, r1		// f_4 + f_3
+	add r0, r0, r1		// f_5 + f_4
+	bx lr
 
 
