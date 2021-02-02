8618ac0e236dffdf3c24daa226fe833916d8de40
Makoto <makoto@emurasoft.com>
2021-02-02 15:34:55 -0800

    fib6

--- part1/fib6.s
+++ part1/fib6.s
@@ -1,11 +1,13 @@
-.data:
-	f0: .byte 0
-	f1: .byte 1
+.data
+	f0: .word 0
+	f1: .word 1
 
-.global _start
-_start:
-	ldr r0, =f0
-	ldr r1, =f1
+.global main
+main:
+	ldr r0, addressF0
+	ldr r0, [r0]
+	ldr r1, addressF1
+	ldr r1, [r1]
 	add r0, r0, r1		// f_0 + f_1
 	add r1, r0, r1		// f_2 + f_1
 	add r0, r0, r1		// f_3 + f_2
@@ -13,4 +15,6 @@ _start:
 	add r0, r0, r1		// f_5 + f_4
 	bx lr
 
+addressF0: .word f0
+addressF1: .word f1
 
